b47556531af59cf700deb11680dcc4c17c5526f8
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-04-11 17:57:23 +0900

    localstorageなしで実装

---
+++ main.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: '#app',
+  data: () => ({
+    newItem: '',
+    todos: []
+  }),
+  methods: {
+    addItem: function(event) {
+      // console.log('Clicked!')
+      if(this.newItem === '') return
+      let todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    },
+    deleteItem: function(index) {
+      // console.log('Delete!')
+      // console.log(index)
+      this.todos.splice(index, 1)
+    }
+  }
+})
